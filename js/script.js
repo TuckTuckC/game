239a86830764ec8c9597c7bce933bd127e76f806
Tucker Craig <tuckercraig@Tuckers-Mac-mini.local>
2021-01-06 22:54:21 -0500

    scoring now works, and added the first stage to making the log roll faster as the game goes on

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 
-player = document.getElementById("player")
-log = document.getElementById("log")
-endScreen = document.getElementById("end-screen")
-$score = $("#score")
+player = document.getElementById("player");
+log = document.getElementById("log");
+endScreen = document.getElementById("end-screen");
+helpScreen = document.getElementById("help-screen");
+$score = $("#score");
 
-lives = 3
-score = 0
+lives = 3;
+score = 0;
 shouldTakeHit = true;
-shouldTakeScore = true;
-SHOULD_TAKE_HIT_TIMER = 400
-SHOULD_TAKE_SCORE_TIMER = 800
-GAME_TICK_SPEED = 5
-JUMP_COOLDOWN = 800
-PAUSED = 500
+scoring = false;
+SHOULD_TAKE_HIT_TIMER = 400;
+GAME_TICK_SPEED = 5;
+SCORE_TICK_SPEED = 15;
+JUMP_COOLDOWN = 800;
+PAUSED = 500;
 
 // Jump function
 document.addEventListener('keydown', e => {
@@ -20,52 +21,70 @@ document.addEventListener('keydown', e => {
         return;
     } else {
         if (e.code === 'Space') {
-            player.setAttribute("class", "jumping");
+            player.classList.add("jumping");
             setTimeout(function(){
-                player.removeAttribute("class", "jumping")
+                player.classList.remove("jumping");
             }, JUMP_COOLDOWN);
-        }
-    }
-})
+        };
+    };
+});
 
 // Collision
 collition = setInterval(function() {
     playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
     logLeft = parseInt(window.getComputedStyle(log).getPropertyValue("left"));
     if (logLeft < -15 && logLeft > -124 && playerTop > 392 && shouldTakeHit) {
-        shouldTakeHit = false
-        lives -= 1
-        score -= 1
-        $('#lives').text(`Lives: ${lives}`)
-        // End
+        shouldTakeHit = false;
+        lives -= 1;
+        $('#lives').text(`Lives: ${lives}`);
+        // End of Game
         if (lives <= 0) {
-            endScreen.style.visibility = "visible"
-            $("#end-score").text(`You got a score of: ${score}`)
-            $('#score').text(`Score: ${score}`)
-            log.removeAttribute("class")
-        }
+            endScreen.classList.add("visible");
+            $("#end-score").text(`You got a score of: ${score}`);
+            $('#score').text(`Score: ${score}`);
+            score = 0;
+            log.className = '';
+            scoring = false;
+        };
     } else if (!shouldTakeHit) {
         setTimeout(function() {
-            shouldTakeHit = true
-        }, SHOULD_TAKE_HIT_TIMER)
-    }
-    if (logLeft < -120) {
-        shouldTakeScore = false
-        score += 1
-        $('#score').text(`Score: ${score}`)
-    } else if(!shouldTakeScore) {
-        setTimeout(function() {
-            shouldTakeScore = true
-        }, SHOULD_TAKE_SCORE_TIMER)
-    }
-}, GAME_TICK_SPEED)
+            shouldTakeHit = true;
+        }, SHOULD_TAKE_HIT_TIMER);
+    };
+    if (score > 815) {
+        log.classList.remove("rolling");
+        log.classList.add("rolling2");
+    };
+}, GAME_TICK_SPEED);
+
 
 // Start
 $('button#start').on('click', function() {
-    endScreen.style.visibility = "hidden"
-    log.setAttribute("class", "rolling")
-    lives = 3
-    score = 0
-    $('#lives').text(`Lives: ${lives}`)
-    $('#score').text(`Score: ${score}`)
-})
+    endScreen.classList.remove("visible");
+    endScreen.classList.add("hidden");
+    helpScreen.classList.remove("visible");
+    helpScreen.classList.add("hidden");
+    log.classList.add("rolling");
+    lives = 3;
+    score = 0;
+    $('#lives').text(`Lives: ${lives}`);
+    $('#score').text(`Score: ${score}`);
+    scoring = true;
+});
+
+// Help
+$('button#help').on('click', function() {
+    if (helpScreen.classList.contains("hidden")) {
+        helpScreen.classList.remove("hidden")
+        helpScreen.classList.add("visible");
+    }else if (helpScreen.classList.contains("visible")) {
+        helpScreen.setAttribute("class", "hidden");
+    };
+});
+
+keepScore = setInterval(function(){
+    if (scoring === true) {
+        score += 1;
+        $('#score').text(`Score: ${score}`);
+    };
+}, SCORE_TICK_SPEED);
